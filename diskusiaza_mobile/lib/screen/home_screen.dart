54e806687d98e0f6e38157d490a3d26d33bc7b55
hussainsyachnuralam <husinnuralam07@gmail.com>
2022-06-21 16:23:08 +0700

    update navbar pada homescreen

--- diskusiaza_mobile/lib/screen/home_screen.dart
+++ diskusiaza_mobile/lib/screen/home_screen.dart
@@ -16,10 +16,38 @@ class _HomeScreenState extends State<HomeScreen> {
         actions: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Icon(Icons.search),
+            child: Icon(Icons.notifications),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.mail),
           )
         ],
       ),
+      body: Center(
+        child: Text(
+          'Home',
+          style: TextStyle(fontSize: 60),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.blue),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+            backgroundColor: Colors.blue),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Trending',
+            backgroundColor: Colors.blue),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Account',
+            backgroundColor: Colors.blue),
+      ]),
     );
   }
 }
